202c722a575449f24cc043c654f105277a5cb0d6
dangwin <danghwin@gmail.com>
2021-05-26 19:59:54 -0500

    commit tree 220cca15b380d012166e0ace53c77f087c341bfd

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReview = props => {
+    const list = props.reviews.map((review, i)=> {
+        return (
+            <li key={i} className='review'>
+                <h3>{review.display_title}</h3>
+                <p>{review.summary_short}</p>
+            </li>
+        )
+    })
+    return (
+        <ul className='review-list'>
+            {list}
+        </ul>
+    )
+}
+
+export default MovieReview
